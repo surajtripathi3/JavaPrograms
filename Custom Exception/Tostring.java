1f81bbaf6519b8aa0f53097344f15334e2f971de
Suraj Tripathi <surajtripathi93@gmail.com>
2012-07-06 10:25:30 +0530

    Use of toString Method Of Object Class

---
+++ Custom Exception/Tostring.java
@@ -0,0 +1,21 @@
+// Use of toString Method to print our message
+
+class Tostring
+{
+
+public String toString()
+{
+
+return "our message:";
+
+}
+
+public static void main(String... s)
+{
+
+Tostring t=new Tostring();
+System.out.println(t);
+
+}
+
+}
